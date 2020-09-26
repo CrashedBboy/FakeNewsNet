80214c44ce041cc2da3a80890a4c7b47363e62a1
CrashedBboy <crashedbboy@gmail.com>
2020-09-26 15:25:18 +0800

    scripts to generate reply/quote mapping

--- code/scripts/hop1_quote_counter.py
+++ code/scripts/hop1_quote_counter.py
@@ -4,32 +4,38 @@ from os import path
 import json
 
 NEWS_DIR = "../fakenewsnet_dataset/politifact/fake"
-RESULT = "../fakenewsnet_dataset/politifact/fake/hop1_quote_list.json"
+news_count = 0
 
-quote_list = []
 for item in os.listdir(NEWS_DIR):
     news_dir_path = path.abspath( path.join(path.dirname(__file__), NEWS_DIR, item) )
     
     if path.isdir(news_dir_path):
+
+        news_count += 1
+        print(f"{news_count}: {item}")
+
         news_path = path.join(news_dir_path, "news content.json")
         if path.exists(news_path):
-
             tweet_dir_path = path.join(news_dir_path, "tweets")
             if path.exists(tweet_dir_path):
-
-                for tweet_filename in os.listdir(tweet_dir_path):
+                quote_map = []
+                tweet_filenames = os.listdir(tweet_dir_path)
+                for tweet_filename in tweet_filenames:
                     if tweet_filename == "first_date.json":
                         continue
 
                     with open(path.join(tweet_dir_path, tweet_filename), "r") as tweet_file:
                         tweet_dict = json.loads(tweet_file.read())
 
-                        if tweet_dict["is_quote_status"] != False:
-                            quote_list.append(tweet_dict)
+                        if tweet_dict["is_quote_status"] == True and "quoted_status" in tweet_dict:
 
-print(f"total quote tweets: {len(quote_list)}")
+                            quoted_tweet_id = tweet_dict["quoted_status"]["id"]
 
-result_file_path = path.abspath( path.join(path.dirname(__file__), RESULT) )
+                            if f"{quoted_tweet_id}.json" in tweet_filenames:
+                                quote_map.append({ 'quoting': tweet_dict['id'], 'quoted': quoted_tweet_id })
+                
+                if len(quote_map) > 0:
+                    print(f"news {item} has {len(quote_map)} pairs of quoting tweets")
 
-with open(result_file_path, "w") as result_file:
-    result_file.write(json.dumps({'count': len(quote_list), 'quote_tweet': quote_list}, indent=4))
+                with open(f"{news_dir_path}/tweet_quote_map.json", "w") as map_file:
+                    map_file.write(json.dumps({ 'count': len(quote_map), 'map': quote_map }))
--- code/scripts/hop1_reply_counter.py
+++ code/scripts/hop1_reply_counter.py
@@ -4,32 +4,37 @@ from os import path
 import json
 
 NEWS_DIR = "../fakenewsnet_dataset/politifact/fake"
-RESULT = "../fakenewsnet_dataset/politifact/fake/hop1_reply_list.json"
+news_count = 0
 
-reply_list = []
 for item in os.listdir(NEWS_DIR):
     news_dir_path = path.abspath( path.join(path.dirname(__file__), NEWS_DIR, item) )
     
     if path.isdir(news_dir_path):
+
+        news_count += 1
+        print(f"{news_count}: {item}")
+
         news_path = path.join(news_dir_path, "news content.json")
         if path.exists(news_path):
 
             tweet_dir_path = path.join(news_dir_path, "tweets")
             if path.exists(tweet_dir_path):
 
-                for tweet_filename in os.listdir(tweet_dir_path):
+                reply_list = []
+                tweet_filenames = os.listdir(tweet_dir_path)
+                for tweet_filename in tweet_filenames:
                     if tweet_filename == "first_date.json":
                         continue
 
                     with open(path.join(tweet_dir_path, tweet_filename), "r") as tweet_file:
                         tweet_dict = json.loads(tweet_file.read())
 
-                        if tweet_dict["in_reply_to_status_id"] != None:
-                            reply_list.append(tweet_dict)
+                        replied_tweet_id = tweet_dict["in_reply_to_status_id"]
+                        if replied_tweet_id != None and f"{replied_tweet_id}.json" in tweet_filenames:
+                            reply_list.append({'replying': tweet_dict['id'], 'replied': replied_tweet_id})
 
-print(f"total reply tweets: {len(reply_list)}")
+                if len(reply_list) > 0:
+                    print(f"news {item} has {len(reply_list)} pairs of replying tweet")
 
-result_file_path = path.abspath( path.join(path.dirname(__file__), RESULT) )
-
-with open(result_file_path, "w") as result_file:
-    result_file.write(json.dumps({'count': len(reply_list), 'reply_tweet': reply_list}, indent=4))
+                with open(f"{news_dir_path}/tweet_reply_map.json", "w") as map_file:
+                    map_file.write(json.dumps({'count': len(reply_list), 'map': reply_list}))
